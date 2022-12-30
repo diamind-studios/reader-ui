fc41d1b0b59d457cadfd8da53e3c135826e4cc38
Mark <diamindstudios@gmail.com>
2022-12-29 23:25:14 -0700

    Format jsx and add topbar

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,12 @@
+export const TopBar = () => {
+  return (
+    <header className='fixed top-0 left-0 h-14 w-screen flex flex-row shadow-m bg-gray-400'>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+        Learn React
+      </a>
+    </header>
+  );
+};

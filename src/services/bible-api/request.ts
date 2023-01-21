ef3b97fb44deafcc1bf6c962e9f1c83e77e08f4f
diamind <diamindstudios@gmail.com>
2023-01-20 20:49:01 -0700

    update dependencies & remove axios header type

--- src/services/bible-api/request.ts
+++ src/services/bible-api/request.ts
@@ -7,7 +7,7 @@ const url = process.env.REACT_APP_API_URL;
 
 export const sendRequest = async (params: any, endpoint: string) => {
   console.log('requesting to:', url + endpoint);
-  const config: AxiosRequestConfig = { headers, params };
+  const config = { headers, params };
   const response = await axios.get(url + endpoint, config);
   return response.data.data;
 };
